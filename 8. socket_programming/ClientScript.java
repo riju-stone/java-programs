5407abdfc19ba6a2149037a282625ebfe481993e
Arighna <riju23chakra@gmail.com>
2021-03-10 15:48:01 +0530

    added username

--- 8. socket_programming/ClientScript.java
+++ 8. socket_programming/ClientScript.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.concurrent.TimeUnit;
 
 public class ClientScript {
-    public static void main(String args[]) throws IOException{
+    public static void main(String args[]) throws UnknownHostException, IOException{
 
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 
@@ -14,25 +14,24 @@ public class ClientScript {
         BufferedReader client_in = null;
 
         //For sending the client messeges to server
-        DataOutputStream client_out = null;
+        BufferedWriter client_out = null;
 
-        String ip = null;
         int port;
-
-        System.out.println("Enter server IP");
-        ip = stdin.readLine();
 
         //Both server and client port numbers should be the same for a successful connection
         System.out.println("Enter your communication port");
         port = Integer.parseInt(stdin.readLine());
 
-        System.out.println("Establishing connection...");
+        String mssg = "";
+        String mssg_received = "";
+
+        System.out.println("[ Establishing connection ]");
 
         try{
-            TimeUnit.SECONDS.sleep(2);
-            socket = new Socket(ip, port);
+            TimeUnit.SECONDS.sleep(1);
+            socket = new Socket("127.0.0.1", port);
             client_in = new BufferedReader(new InputStreamReader(System.in));
-            client_out = new DataOutputStream(socket.getOutputStream());
+            client_out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         }
         catch(InterruptedException interrupt){
             System.out.println("Connection process interrupted...");
@@ -41,16 +40,28 @@ public class ClientScript {
             System.out.println("Couldn\'t locate mentioned host...");
         }
 
-        System.out.println("Connection Established...");
-        String mssg = "";
+        System.out.println("[ Connection Established ]");
+        System.out.println("Enter Username");
+        String username = client_in.readLine();
+        client_out.write(username);
+        client_out.newLine();
+        client_out.flush();
+        System.out.println("[ Username Synched with Server ]");
+        System.out.println("Enter Your Message...");
 
-        while(!mssg.equalsIgnoreCase("disconnect")){
-            mssg = client_in.readLine();
-            client_out.writeUTF(mssg);
+        while(!mssg.equalsIgnoreCase("!disconnect")){
+            mssg = "";
+            mssg = stdin.readLine();
+            client_out.write(mssg);
+            client_out.newLine();
+            client_out.flush();
+            // mssg_received = client_in.readLine();
+            // System.out.println("[ "+mssg_received+" ]");
         }
 
-        System.out.println("Disconnected from the server...");
+        System.out.println("[ Disconnected from the server ]");
         socket.close();
         client_in.close();
+        client_out.close();
     }
 }
--- 8. socket_programming/ServerScript.java
+++ 8. socket_programming/ServerScript.java
@@ -2,7 +2,7 @@ import java.net.*;
 import java.io.*;
 
 public class ServerScript {
-    public static void main(String args[]) throws IOException{
+    public static void main(String args[]) throws IOException, SocketTimeoutException{
 
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 
@@ -10,39 +10,50 @@ public class ServerScript {
         ServerSocket server_socket = null;
 
         //Client type socket for establishing connection with the server
-        Socket client_socket;
+        Socket connection_socket = null;
 
         //For receiving messages from the client
-        DataInputStream server_in = null;
+        BufferedReader server_in = null;
 
+        //For sending output to the client
+        BufferedWriter server_out = null;
+
+        String client_username = "";
         int port;
 
         System.out.println("Enter you connection port");
         port = Integer.parseInt(stdin.readLine());
 
+        System.out.println("[ Server Booted ]");
+        System.out.println("[ Listening for Clients ]");
+        server_socket = new ServerSocket(port);
         try{
-            server_socket = new ServerSocket(port);
-            System.out.println("Server Booted");
-            System.out.println("Listening for Clients...");
-            client_socket = server_socket.accept();
-            server_in = new DataInputStream(client_socket.getInputStream());
+            connection_socket = server_socket.accept();
+            server_in = new BufferedReader(new InputStreamReader(connection_socket.getInputStream()));
+            server_out = new BufferedWriter(new OutputStreamWriter(connection_socket.getOutputStream()));
         } 
-        catch( IOException e){
+        catch(IOException e){
             System.out.println("Could not establish connection using mentioned port number");
         }
 
-        System.out.println("Connection Established with Client");
-
+        System.out.println("[ Connection Established with Client ]");
 
         String client_mssg = "";
 
-        while(!client_mssg.equalsIgnoreCase("disconnect")){
-            client_mssg = server_in.readUTF();
-            System.out.println("Client : "+ client_mssg);
+        client_mssg = server_in.readLine();
+        client_username = client_mssg;
+        client_mssg = "";
+
+        while(!client_mssg.equalsIgnoreCase("!disconnect")){
+            client_mssg = server_in.readLine();
+            System.out.println(client_username+" : "+ client_mssg);
+            // server_out.write("[ Server : Message Received ]");
+            // server_out.flush();
         }
 
-        System.out.println("Client disconnected");
+        System.out.println("[ "+client_username+" disconnected ]");
         server_socket.close();
         server_in.close();
+        server_out.close();
     }
 }
